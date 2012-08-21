5d9c0346b6cb8955a0daec75a37bda5de4dcd8d8
Keith Strand <keith.strand@gmail.com>
2012-08-21 16:26:52 -0400

    text font changed

---
+++ app/controllers/profile_pages_controller.rb
@@ -0,0 +1,4 @@
+class ProfilePagesController < ApplicationController
+  def home
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 DbcProfile::Application.routes.draw do
+  # get "profile_pages/home"
+  root :to => 'profile_pages#home'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
